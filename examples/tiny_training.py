f87c370a6dd8d1bce6b89d8cd37b0638f2a304e2
meenchen <b98902022@ntu.edu.tw>
2022-11-27 14:29:07 -0500

    README

--- examples/tiny_training.py
+++ examples/tiny_training.py
@@ -17,8 +17,8 @@
 # ----------------------------------------------------------------------
 # Example commands:
 # cd examples
-# python tiny_training.py -f ../assets/49kb-int8-graph.json -D ../assets/full-int8-params.pkl -QAS \
-# ../assets/scale.json -m -g -d -FR
+# python examples/tiny_training.py -f assets/49kb-int8-graph.json -D assets/full-int8-params.pkl -QAS \
+# assets/scale.json -m -g -d -FR
 #
 
 import argparse
