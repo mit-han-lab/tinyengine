084ed9e70d7f6839b628ad77019a7f5f019d0f28
meenchen <b98902022@ntu.edu.tw>
2023-01-16 13:15:42 -0500

    fix function def

--- TinyEngine/include/tinyengine_function.h
+++ TinyEngine/include/tinyengine_function.h
@@ -151,7 +151,7 @@ tinyengine_status patchpadding_convolve_s8_kernel3_inputch3_stride2(
     const q7_t *input, const uint16_t input_x, const uint16_t input_y, const uint16_t input_ch, const q7_t *kernel,
     const int32_t *bias, const int32_t *output_shift, const int32_t *output_mult, const int32_t output_offset,
     const int32_t input_offset, const int32_t output_activation_min, const int32_t output_activation_max, q7_t *output,
-    const uint16_t output_x, const uint16_t output_y, const uint16_t output_ch, q15_t *runtime_buf,
+    const uint16_t output_x, const uint16_t output_y, const uint16_t output_ch, q15_t *runtime_buf, q15_t *kbuf,
     q7_t pad_value, const uint16_t pad_t, const uint16_t pad_b, const uint16_t pad_l, const uint16_t pad_r);
 
 tinyengine_status patchpadding_depthwise_kernel3x3_stride1_inplace_CHW(
