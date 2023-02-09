24a8dd337ea2bcaa8ee39de4df2b89475c5c4249
meenchen <b98902022@ntu.edu.tw>
2023-02-08 23:38:13 -0500

    python code for openmv IDE

---
+++ examples/openmv_person_detection/person_detection_demo.py
@@ -0,0 +1,15 @@
+# This example shows how to invoke training or inference function calls of tinyengine.
+import cexample
+import lcd
+import sensor
+
+sensor.reset()  # Reset and initialize the sensor.
+sensor.set_pixformat(sensor.RGB565)  # Set pixel format to RGB565 (or GRAYSCALE)
+sensor.set_framesize(sensor.LCD)  # Set frame size to QVGA 160x120
+lcd.init()  # Initialize the lcd screen.
+
+while True:
+    img = sensor.snapshot()  # Take a picture and return the image.
+
+    ret = cexample.person_detection(img, 0.15)
+    lcd.display(img)  # Display the image.
