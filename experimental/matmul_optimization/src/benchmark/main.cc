9e8d6373f13bf709653383da075121b31aa739ca
meenchen <meenchen79@gmail.com>
2023-04-09 18:52:01 -0400

    int8 onednn

--- experimental/matmul_optimization/src/benchmark/main.cc
+++ experimental/matmul_optimization/src/benchmark/main.cc
@@ -1,9 +1,12 @@
 #include <math.h>
 #include <stdio.h>
 
+#include <cstdlib>
+#include <iostream>
+
 #include "lib/matmul.h"
 
-#define BLK_SIZE 32
+#define BLK_SIZE 64
 #define MAX_PRECISION_ERROR 0.01
 
 #define A_ROW 1024
@@ -12,6 +15,12 @@
 #define B_COLUMN 1024
 #define C_ROW 1024
 #define C_COLUMN 1024
+// #define A_ROW 2
+// #define A_COLUMN 2048
+// #define B_ROW 2048
+// #define B_COLUMN 2
+// #define C_ROW 2
+// #define C_COLUMN 2
 #define NUM_THREAD 4
 
 float MAT_A[A_ROW * A_COLUMN];
@@ -19,6 +28,12 @@ float MAT_B[B_ROW * B_COLUMN];
 float transpose_B[B_ROW * B_COLUMN];
 float native_C[C_ROW * C_COLUMN];
 float output_C[C_ROW * C_COLUMN];
+
+int8_t MAT_A_s8[A_ROW * A_COLUMN];
+int8_t MAT_B_s8[B_ROW * B_COLUMN];
+int8_t transpose_B_s8[B_ROW * B_COLUMN];
+int8_t native_C_s8[C_ROW * C_COLUMN];
+int8_t output_C_s8[C_ROW * C_COLUMN];
 
 bool check_identical(float matA[], float matB[], int size) {
     for (int i = 0; i < size; i++) {
@@ -30,9 +45,34 @@ bool check_identical(float matA[], float matB[], int size) {
     return true;
 }
 
+bool check_identical(int8_t matA[], int8_t matB[], int size) {
+    for (int i = 0; i < size; i++) {
+        if (matA[i] != matB[i]) {
+            printf("%d: %d, %d", i, matA[i], matB[i]);
+            return false;
+        }
+    }
+    return true;
+}
+
+template <typename T>
+void dump_integer_array(T matA[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d,", matA[i]);
+    }
+    printf("\n");
+}
+
 void initialize_matrix(float A[], int size) {
     for (int i = 0; i < size; i++) {
         A[i] = (float)(rand()) / (float)(RAND_MAX);
+    }
+}
+
+void initialize_matrix(int8_t A[], int size) {
+    for (int i = 0; i < size; i++) {
+        // A[i] = (rand() % 2) - 1;
+        A[i] = (rand() % 2);
     }
 }
 
@@ -44,9 +84,13 @@ int main() {
     initialize_matrix(MAT_B, B_ROW * B_COLUMN);
     initialize_matrix(native_C, C_ROW * C_COLUMN);
 
+    initialize_matrix(MAT_A_s8, A_ROW * A_COLUMN);
+    initialize_matrix(MAT_B_s8, B_ROW * B_COLUMN);
+    initialize_matrix(native_C_s8, C_ROW * C_COLUMN);
+
     MatmulOperator matmul_op = MatmulOperator();
 
-    struct matmul_params params;
+    struct matmul_params params, params_int8;
     params.A.row = A_ROW;
     params.A.column = A_COLUMN;
     params.A.data_ptr = MAT_A;
@@ -57,6 +101,25 @@ int main() {
     params.C.column = C_COLUMN;
     params.opt_params.blk_size = BLK_SIZE;
     params.opt_params.num_thread = NUM_THREAD;
+
+    // int8
+    params_int8.A.row = A_ROW;
+    params_int8.A.column = A_COLUMN;
+    params_int8.A.int8_data_ptr = MAT_A_s8;
+    params_int8.A.qparams.scale = 1.0;
+    params_int8.A.qparams.zero_point = 0;
+    params_int8.B.row = B_ROW;
+    params_int8.B.column = B_COLUMN;
+    params_int8.B.int8_data_ptr = MAT_B_s8;
+    params_int8.B.qparams.scale = 1.0;
+    params_int8.B.qparams.zero_point = 0;
+    params_int8.C.row = C_ROW;
+    params_int8.C.column = C_COLUMN;
+    params_int8.C.int8_data_ptr = native_C_s8;
+    params_int8.C.qparams.scale = 1.0;
+    params_int8.C.qparams.zero_point = 0;
+    params_int8.opt_params.blk_size = BLK_SIZE;
+    params_int8.opt_params.num_thread = NUM_THREAD;
 
     // Baseline
     params.C.data_ptr = native_C;
@@ -107,13 +170,27 @@ int main() {
     params.B.column = B_ROW;
     params.B.row = B_COLUMN;
     params.B.data_ptr = transpose_B;
-    params.opt_params.blk_size = 4;
-    params.opt_params.num_thread = 4;
+    params.opt_params.blk_size = BLK_SIZE;
+    params.opt_params.num_thread = NUM_THREAD;
 
     // fast
     initialize_matrix(output_C, C_ROW * C_COLUMN);
     matmul_op.evaluate(MatmulOperator::FAST, &params);
     if (!check_identical(native_C, output_C, C_ROW * C_COLUMN)) printf("incorrect output of mat_mul_fast\n");
+
+    // int8
+    matmul_op.evaluate(MatmulOperator::INT8_BASELINE, &params_int8);
+
+    params_int8.C.int8_data_ptr = output_C_s8;
+    matmul_op.evaluate(MatmulOperator::ONEDNN_INT8, &params_int8);
+    if (!check_identical(native_C_s8, output_C_s8, C_ROW * C_COLUMN))
+        printf("incorrect output from mat_mul_unrolling\n");
+
+    // Debugging
+    // dump_integer_array(MAT_A_s8, A_ROW * A_COLUMN);
+    // dump_integer_array(MAT_B_s8, B_ROW * B_COLUMN);
+    // dump_integer_array(native_C_s8, C_ROW * C_COLUMN);
+    // dump_integer_array(output_C_s8, C_ROW * C_COLUMN);
 
     return 0;
 }
--- experimental/matmul_optimization/src/lib/matmul_onednn.cc
+++ experimental/matmul_optimization/src/lib/matmul_onednn.cc
@@ -69,5 +69,72 @@ void MatmulOperator::mat_mul_onednn(const struct matmul_params *params) {
     int us = interval_to_us(&start, &end);
     std::cout << "onednn kernel: " << us / 1000 << " ms" << std::endl;
 }
+
+void MatmulOperator::mat_mul_onednn_int8(const struct matmul_params *params) {
+    const struct matrix *A = &params->A, *B = &params->B, *C = &params->C;
+    int8_t *data_A = A->int8_data_ptr, *data_B = B->int8_data_ptr, *data_C = C->int8_data_ptr;
+    CHECK_MATRICES(A, B, C);
+
+    int M = A->row, N = B->column, K = A->column;
+
+    // Initialize description for matmul
+    dnnl::memory::desc a_md({M, K}, dnnl::memory::data_type::s8, {K, 1});  // M x K layout
+    dnnl::memory::desc b_md({K, N}, dnnl::memory::data_type::s8, {N, 1});  // K X N layout
+    dnnl::memory::desc c_md({M, N}, dnnl::memory::data_type::s8, {N, 1});  // M x N layout
+
+    dnnl::engine eng(dnnl::engine::kind::cpu, 0);
+    dnnl::primitive_attr attr;
+    attr.set_scales_mask(DNNL_ARG_SRC, /* mask */ 0);
+    attr.set_scales_mask(DNNL_ARG_WEIGHTS, /* mask */ 1 << 1);
+    attr.set_scales_mask(DNNL_ARG_DST, /* mask */ 0);
+    attr.set_zero_points_mask(DNNL_ARG_SRC, /* mask */ 0);
+    attr.set_zero_points_mask(DNNL_ARG_DST, /* mask */ 0);
+    auto matmul_desc = dnnl::matmul::primitive_desc(eng, a_md, b_md, c_md, attr);
+
+    // Initialize memory
+    dnnl::memory A_mem(matmul_desc.src_desc(), eng, (void *)data_A);
+    dnnl::memory B_mem(matmul_desc.weights_desc(), eng, (void *)data_B);
+    dnnl::memory C_mem(matmul_desc.dst_desc(), eng, (void *)data_C);
+
+    dnnl::memory zp_A_mem({{1}, dnnl::memory::data_type::s32, {1}}, eng);
+    dnnl::memory zp_C_mem({{1}, dnnl::memory::data_type::s32, {1}}, eng);
+    dnnl::memory sc_A_mem({{1}, dnnl::memory::data_type::f32, {1}}, eng);
+    dnnl::memory sc_B_mem({{N}, dnnl::memory::data_type::f32, {1}}, eng);
+    dnnl::memory sc_C_mem({{1}, dnnl::memory::data_type::f32, {1}}, eng);
+
+    // Assign zero points
+    int32_t *zp_handle = static_cast<int32_t *>(zp_A_mem.get_data_handle());
+    *zp_handle = A->qparams.zero_point;
+    zp_handle = static_cast<int32_t *>(zp_C_mem.get_data_handle());
+    *zp_handle = C->qparams.zero_point;
+
+    // Assign scales
+    float *sc_handle = static_cast<float *>(sc_A_mem.get_data_handle());
+    *sc_handle = A->qparams.scale;
+    sc_handle = static_cast<float *>(sc_B_mem.get_data_handle());
+    for (int i = 0; i < N; i++) sc_handle[i] = B->qparams.scale;
+    sc_handle = static_cast<float *>(sc_C_mem.get_data_handle());
+    *sc_handle = C->qparams.scale;
+
+    // Operator
+    dnnl::matmul matmul_p(matmul_desc);
+
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
+    dnnl::stream s(eng);
+    matmul_p.execute(s, {{DNNL_ARG_SRC, A_mem},
+                         {DNNL_ARG_WEIGHTS, B_mem},
+                         {DNNL_ARG_DST, C_mem},
+                         {DNNL_ARG_ATTR_SCALES | DNNL_ARG_SRC, sc_A_mem},
+                         {DNNL_ARG_ATTR_SCALES | DNNL_ARG_WEIGHTS, sc_B_mem},
+                         {DNNL_ARG_ATTR_SCALES | DNNL_ARG_DST, sc_C_mem},
+                         {DNNL_ARG_ATTR_ZERO_POINTS | DNNL_ARG_SRC, zp_A_mem},
+                         {DNNL_ARG_ATTR_ZERO_POINTS | DNNL_ARG_DST, zp_C_mem}});
+    s.wait();
+    gettimeofday(&end, NULL);
+    int us = interval_to_us(&start, &end);
+    std::cout << "onednn kernel: " << us / 1000 << " ms" << std::endl;
+}
+
 }  // namespace matmul
 #endif
