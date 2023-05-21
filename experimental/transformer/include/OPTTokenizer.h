3813bb91a583ec26998e71c7f4db298dfc8b5cbf
RaymondWang0 <wweichen@mit.edu>
2023-05-21 05:37:43 -0400

    add OPT tokenizer

--- experimental/transformer/include/OPTTokenizer.h
+++ experimental/transformer/include/OPTTokenizer.h
@@ -1,3 +1,6 @@
+#ifndef OPT_TOKENIZER_H
+#define OPT_TOKENIZER_H
+
 #include <cstdio>
 #include <vector>
 #include <string>
@@ -14,10 +17,14 @@
 #include <sstream>
 #include <fstream>
 #include <utility>
-//#include <boost/regex.hpp>
-//#include <nlohmann/json.hpp>
+#include <regex>
+#include <boost/regex.hpp>
+#include <nlohmann/json.hpp>
 
-#include "OPT.h"
+//#include "OPT.h"
+
+//std::vector<int> OPT_tokenize(const OPT_vocab & vocab, const std::string & text, bool add_bos);
+
 
 /* TODO */
 inline int n_ctx = 1024;
@@ -35,4 +42,33 @@ inline int OPT_token_nl() {
     return 13;
 }
 
-std::vector<int> OPT_tokenize(const OPT_vocab & vocab, const std::string & text, bool add_bos);
+struct pair_hash {
+    template <class T1, class T2>
+    std::size_t operator () (const std::pair<T1,T2> &p) const {
+        auto h1 = std::hash<T1>{}(p.first);
+        auto h2 = std::hash<T2>{}(p.second); 
+        return h1 ^ h2;
+    }
+};
+
+class Encoder {
+public:
+    Encoder(std::map<std::string, int> encoder, std::vector<std::pair<std::string, std::string>> bpe_merges);
+    std::unordered_map<int, std::string> bytes_to_unicode();
+    std::set<std::pair<std::string, std::string>> get_pairs(std::vector<std::string> word);
+    std::string bpe(std::string token);
+    std::vector<int> encode(std::string text);
+    std::string decode(std::vector<int> tokens);
+
+private:
+    std::map<std::string, int> encoder;
+    std::map<int, std::string> decoder;
+    std::unordered_map<int, std::string> byte_encoder;
+    std::unordered_map<std::string, int> byte_decoder;
+    std::unordered_map<std::pair<std::string, std::string>, int, pair_hash> bpe_ranks;
+    std::unordered_map<std::string, std::string> cache;
+};
+
+Encoder get_encoder(std::string vocab_file, std::string bpe_file);
+
+#endif
--- experimental/transformer/src/OPTTokenizer.cc
+++ experimental/transformer/src/OPTTokenizer.cc
@@ -1,9 +1,9 @@
 #include "OPTTokenizer.h"
 
-std::vector<int> OPT_tokenize(const OPT_vocab & vocab, const std::string & text, bool add_bos) {
+/*std::vector<int> OPT_tokenize(const OPT_vocab & vocab, const std::string & text, bool add_bos) {
     std::vector<int> res(text.size() + (int) add_bos);
     return res; 
-}
+}*/
 
 /*
 // Defining constant map of vocab file names
@@ -48,163 +48,236 @@ std::map<std::string, int> PRETRAINED_POSITIONAL_EMBEDDINGS_SIZES = {
 /* 
  *  Tokenizer 
  */
+Encoder::Encoder(std::map<std::string, int> encoder, std::vector<std::pair<std::string, std::string>> bpe_merges) {
+    this->encoder = encoder;
+    for(auto &it: encoder) {
+        this->decoder[it.second] = it.first;
+    }
+    this->byte_encoder = bytes_to_unicode();
+    for(auto &it: byte_encoder) {
+        this->byte_decoder[it.second] = it.first;
+    }
+    for(int i = 0; i < bpe_merges.size(); ++i) {
+        this->bpe_ranks.insert(std::make_pair(bpe_merges[i], i)); 
+    }
+}
+
 /*
-class Encoder {
-public:
-    Encoder(std::map<std::string, int> encoder, std::vector<std::string> bpe_merges, std::string errors = "replace")
-    : errors(errors) {
-        this->encoder = encoder;
-        for(auto &it: encoder)
-            this->decoder[it.second] = it.first;
-        this->byte_encoder = bytes_to_unicode();
-        for(auto &it: byte_encoder)
-            this->byte_decoder[it.second] = it.first;
-        for(int i = 0; i < bpe_merges.size(); ++i)
-            this->bpe_ranks[bpe_merges[i]] = i;
+Encoder::std::vector<std::pair<int, unsigned char>> bytes_to_unicode() {
+    std::vector<int> bs;
+    std::vector<int> cs;
+
+    for (int i = int(U'!'); i <= int(U'~'); ++i) bs.push_back(i);
+    for (int i = int(U'¡'); i <= int(U'¬'); ++i) bs.push_back(i);
+    for (int i = int(U'®'); i <= int(U'ÿ'); ++i) bs.push_back(i);
+
+    cs = bs;  // Copy bs to cs
+
+    int n = 0;
+    for (int b = 0; b < 256; ++b) {
+        if (std::find(bs.begin(), bs.end(), b) == bs.end()) {
+            bs.push_back(b);
+            cs.push_back(256 + n);
+            ++n;
+        }
     }
 
-    std::map<int, char> bytes_to_unicode() {
-        std::vector<int> bs;
-        std::vector<int> cs;
-        int n = 0;
+    std::vector<std::pair<int, unsigned char>> b2u;
+    for (int i = 0; i < bs.size(); ++i) {
+        b2u.emplace_back(bs[i], static_cast<unsigned char>(bs[i]));
+    }
 
-        for (int i = int('!'); i <= int('~'); ++i) bs.push_back(i);
-        for (int i = int('¡'); i <= int('¬'); ++i) bs.push_back(i);
-        for (int i = int('®'); i <= int('ÿ'); ++i) bs.push_back(i);
+    return b2u;
+}
+*/
 
-        cs = bs;  // Copy bs to cs
+std::unordered_map<int, std::string> Encoder::bytes_to_unicode() {
+    std::unordered_map<int, std::string> byte_to_unicode;
+    
+    // Range from '!' to '~'
+    for (int b = '!'; b <= '~'; ++b) {
+        byte_to_unicode[b] = std::string(1, static_cast<char>(b));
+    }
 
-        for (int b = 0; b < 256; ++b) {
-            if (std::find(bs.begin(), bs.end(), b) == bs.end()) {
-                bs.push_back(b);
-                cs.push_back(256 + n);
-                ++n;
+    // ASCII control characters
+    byte_to_unicode[0x00] = u8"\u0000";  // Null char
+    byte_to_unicode[0x01] = u8"\u0001";  // Start of Heading
+    byte_to_unicode[0x02] = u8"\u0002";  // Start of Text
+    byte_to_unicode[0x03] = u8"\u0003";  // End of Text
+    byte_to_unicode[0x04] = u8"\u0004";  // End of Transmission
+    byte_to_unicode[0x05] = u8"\u0005";  // Enquiry
+    byte_to_unicode[0x06] = u8"\u0006";  // Acknowledge
+    byte_to_unicode[0x07] = u8"\u0007";  // Bell
+    byte_to_unicode[0x08] = u8"\u0008";  // Back Space
+    byte_to_unicode[0x09] = u8"\u0009";  // Horizontal Tab
+    byte_to_unicode[0x0A] = u8"\u000A";  // Line Feed
+    byte_to_unicode[0x0B] = u8"\u000B";  // Vertical Tab
+    byte_to_unicode[0x0C] = u8"\u000C";  // Form Feed
+    byte_to_unicode[0x0D] = u8"\u000D";  // Carriage Return
+    byte_to_unicode[0x0E] = u8"\u000E";  // Shift Out / X-On
+    byte_to_unicode[0x0F] = u8"\u000F";  // Shift In / X-Off
+    byte_to_unicode[0x10] = u8"\u0010";  // Data Line Escape
+    byte_to_unicode[0x11] = u8"\u0011";  // Device Control 1 (oft. XON)
+    byte_to_unicode[0x12] = u8"\u0012";  // Device Control 2
+    byte_to_unicode[0x13] = u8"\u0013";  // Device Control 3 (oft. XOFF)
+    byte_to_unicode[0x14] = u8"\u0014";  // Device Control 4
+    byte_to_unicode[0x15] = u8"\u0015";  // Negative Acknowledgement
+    byte_to_unicode[0x16] = u8"\u0016";  // Synchronous Idle
+    byte_to_unicode[0x17] = u8"\u0017";  // End of Transmit Block
+    byte_to_unicode[0x18] = u8"\u0018";  // Cancel
+    byte_to_unicode[0x19] = u8"\u0019";  // End of Medium
+    byte_to_unicode[0x1A] = u8"\u001A";  // Substitute
+    byte_to_unicode[0x1B] = u8"\u001B";  // Escape
+    byte_to_unicode[0x1C] = u8"\u001C";  // File Separator
+    byte_to_unicode[0x1D] = u8"\u001D";  // Group Separator
+    byte_to_unicode[0x1E] = u8"\u001E";  // Record Separator
+    byte_to_unicode[0x1F] = u8"\u001F";  // Unit Separator
+
+    // Basic ASCII characters
+    byte_to_unicode[0x20] = "Ġ";  // Space
+
+    // Extended ASCII range
+    byte_to_unicode[0xA0] = u8"\u00A0";  // Non-breaking space
+    byte_to_unicode[0xA1] = u8"\u0104";  // Ą
+    byte_to_unicode[0xA2] = u8"\u0138";  // ĸ
+    byte_to_unicode[0xA3] = u8"\u0156";  // Ŗ
+    byte_to_unicode[0xA4] = u8"\u00A4";  // Currency symbol
+    byte_to_unicode[0xA5] = u8"\u0128";  // Ĩ
+    
+    return byte_to_unicode;
+}
+
+std::set<std::pair<std::string, std::string>> Encoder::get_pairs(std::vector<std::string> word) {
+    std::set<std::pair<std::string, std::string>> pairs;
+    std::string prev_string = word[0];
+    for (int i = 1; i < word.size(); ++i) {
+        pairs.insert(std::make_pair(prev_string, word[i]));
+        prev_string = word[i];
+    }
+
+    return pairs;
+}
+
+std::string Encoder::bpe(std::string token) {
+    auto it = this->cache.find(token); // Find the token in the cache
+    if (it != this->cache.end()) { // If the token is in the cache
+        return it->second;
+    }
+
+    std::vector<std::string> word; // word = tuple(token)
+    for (char c : token) {
+        word.push_back(std::string(1, c));
+    }
+
+    std::set<std::pair<std::string, std::string>> pairs = get_pairs(word);
+
+    if(pairs.empty())
+        return token;    
+
+    while(true) {
+        std::pair<std::string, std::string> bigram;
+        int min_index = std::numeric_limits<int>::max(); // Start with the highest possible int value
+
+        for (const auto &pair: pairs) {
+            auto it = this->bpe_ranks.find(pair); // Find the pair in the map
+            if (it != this->bpe_ranks.end()) { // If the pair is in the map
+                if (it->second < min_index) { // If the current pair's value is less than the min_index
+                    min_index = it->second;
+                    bigram = pair;
+                }
             }
         }
 
-        for (int& n : cs) n = char(n);  // Convert int to char
+        if (min_index == std::numeric_limits<int>::max()) // No pair was found in bpe_ranks
+            break;
 
-        std::map<int, char> b2u;
-        for (int i = 0; i < bs.size(); ++i) {
-            b2u[bs[i]] = cs[i];
-        }
-
-        return b2u;
-    }
-
-    std::set<std::pair<std::string, std::string>> get_pairs(std::string word){
-        std::set<std::pair<std::string, std::string>> pairs;
-        char prev_char = word[0];
-        for (int i = 1; i < word.length(); ++i) {
-            pairs.insert(std::make_pair(prev_char, word[i]));
-            prev_char = word[i];
-        }
-
-        return pairs;
-    }
-
-    std::string bpe(std::string token) {
-        if(this->cache.find(token) != this->cache.end())
-            return this->cache[token];
-        std::vector<std::string> word (token.begin(), token.end());
-        std::set<std::pair<std::string, std::string>> pairs = get_pairs(token);
-
-        if(pairs.empty())
-            return token;
-
-        while(true) {
-            std::pair<std::string, std::string> bigram = *pairs.begin();
-            for(auto &pair: pairs)
-                if(this->bpe_ranks[pair] < this->bpe_ranks[bigram])
-                    bigram = pair;
-            if(this->bpe_ranks.find(bigram) == this->bpe_ranks.end())
+        std::string first = bigram.first;
+        std::string second = bigram.second;
+        std::vector<std::string> new_word;
+        int i = 0;
+        while(i < word.size()) {
+            auto it = std::find(word.begin() + i, word.end(), first);
+            if (it == word.end()) {
+                new_word.insert(new_word.end(), word.begin() + i, word.end());
                 break;
+            }
+            int j = it - word.begin();
+            new_word.insert(new_word.end(), word.begin() + i, word.begin() + j);
+            i = j;
 
-            std::string first = bigram.first;
-            std::string second = bigram.second;
-            std::vector<std::string> new_word;
-            int i = 0;
-            while(i < word.size()) {
-                auto j = std::find(word.begin() + i, word.end(), std::string(1, first));
-                if(j == word.end()) {
-                    new_word.insert(new_word.end(), word.begin() + i, word.end());
+            if (word[i] == first && i < word.size() - 1 && word[i + 1] == second) {
+                new_word.push_back(first + second);
+                i += 2;
+            }
+            else {
+                new_word.push_back(word[i]);
+                i += 1;
+            }
+        }
+
+        word = new_word;
+
+        if (word.size() == 1)
+            break;
+        else
+            pairs = get_pairs(word);
+    }
+
+    this->cache.insert(std::make_pair(token, std::accumulate(word.begin(), word.end(), std::string())));
+
+    return token;
+}
+
+std::vector<int> Encoder::encode(std::string text) {
+    std::vector<int> bpe_tokens;
+
+    // Using Regex to tokenize
+    std::regex pat = std::regex("'s|'t|'re|'ve|'m|'ll|'d| ?[a-zA-Z]+| ?[0-9]+| ?[^\\s\\p{L}\\p{N}]+|\\s+(?!\\S)|\\s+");
+    std::sregex_iterator iter(text.begin(), text.end(), pat);
+    std::sregex_iterator end;
+
+    while (iter != end) {
+        std::string token = iter->str();
+        std::string encoded_token;
+
+        for (char b : token) {
+            for (auto &it: this->byte_encoder) {
+                if (it.first == int(static_cast<int>(b))) {
+                    encoded_token += it.second;
                     break;
                 }
-                new_word.insert(new_word.end(), word.begin() + i, j);
-                i = std::distance(word.begin(), j);
-
-                if(word[i] == std::string(1, first) && i < word.size() - 1 && word[i + 1] == std::string(1, second)) {
-                    new_word.push_back(std::string(1, first) + second);
-                    i += 2;
-                } else {
-                    new_word.push_back(word[i]);
-                    i += 1;
-                }
-            }
-            word = new_word;
-
-            if (word.size() == 1)
-                break;
-            else
-                pairs = get_pairs(std::string(word.begin(), word.end()));
-        }
-
-        token = std::accumulate(word.begin(), word.end(), std::string(), [](std::string &s1, std::string &s2) {
-            return s1 + ' ' + s2;
-        });
-        this->cache[token] = token;
-
-        return token;
-    }
-
-    std::vector<int> encode(std::string text) {
-        std::vector<int> bpe_tokens;
-
-        // Using Boost Regex to tokenize
-        boost::regex pat(R"('s|'t|'re|'ve|'m|'ll|'d| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+)");
-        boost::sregex_iterator it(text.begin(), text.end(), pat);
-        boost::sregex_iterator end;
-
-        for (;it != end; ++it) {
-            std::string token = it->str();
-
-            std::string utf8_token;
-            for (unsigned char c : token) {
-                utf8_token += byte_encoder[c];
-            }
-
-            std::string bpe_result = bpe(utf8_token);
-            std::istringstream iss(bpe_result);
-            std::vector<std::string> results((std::istream_iterator<std::string>(iss)),
-                                            std::istream_iterator<std::string>());
-            for (const std::string& bpe_token : results) {
-                bpe_tokens.push_back(encoder[bpe_token]);
             }
         }
 
-        return bpe_tokens;
+        bpe_tokens.push_back(encoder[encoded_token]);
+        ++iter;
     }
 
-    std::string decode(std::vector<int> tokens) {
-        std::string text;
-        for (int &token : tokens) {
-            if (this->decoder.find(token) != this->decoder.end())
-                text += this->byte_decoder[this->decoder[token][0]];
+    return bpe_tokens;
+}
+
+std::string Encoder::decode(std::vector<int> tokens) {
+    std::string text;
+    for (const auto &token : tokens) {
+        if (this->decoder.find(token) != this->decoder.end()) {
+            // TODO: currently using brutal method. Need to optimize.
+            int i_flag = 1;
+            if (int(this->decoder[token][0]) < '!' || int(this->decoder[token][0]) > '~') {
+                text += " ";
+                i_flag = 2;
+            }
+            else {
+                text += std::string(1, this->decoder[token][0]);
+            }
+
+            for (int i = i_flag; i < this->decoder[token].size(); ++i) {
+                text += std::string(1, this->decoder[token][i]);
+            }
         }
-        std::wstring_convert<std::codecvt_utf8<char32_t>, char32_t> converter;
-        std::string converted = converter.to_bytes(text);
-        return converted;
     }
 
-private:
-    std::map<std::string, int> encoder;
-    std::map<int, std::string> decoder;
-    std::string errors;
-    std::map<int, char> byte_encoder;
-    std::map<char, int> byte_decoder;
-    std::map<std::pair<char, char>, int> bpe_ranks;
-    std::map<std::string, std::string> cache;
-};
+    return text;
+}
 
 Encoder get_encoder(std::string vocab_file, std::string bpe_file) {
     std::ifstream i(vocab_file);
@@ -216,6 +289,10 @@ Encoder get_encoder(std::string vocab_file, std::string bpe_file) {
     std::ifstream infile(bpe_file);
     std::string line;
     std::vector<std::pair<std::string, std::string>> bpe_merges;
+    // Read and ignore the first line.
+    if (!std::getline(infile, line)) {
+        // TODO: Handle the error (for example, file might be empty).
+    }
     while (std::getline(infile, line)) {
         std::istringstream iss(line);
         std::string a, b;
@@ -225,4 +302,3 @@ Encoder get_encoder(std::string vocab_file, std::string bpe_file) {
 
     return Encoder(encoder, bpe_merges);
 }
-*/
